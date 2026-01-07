347e0a3df51678081b622e8f6783317715fd816b
Marvin <52848568+mleem97@users.noreply.github.com>
2026-01-07 02:59:32 +0100

    feat: include BotLogger in core module exports

--- Src/rush_bot/core/__init__.py
+++ Src/rush_bot/core/__init__.py
@@ -3,5 +3,6 @@
 from .bot import Bot
 from .handler import BotHandler
 from .device import DeviceManager
+from .logger import BotLogger
 
-__all__ = ["Bot", "BotHandler", "DeviceManager"]
+__all__ = ["Bot", "BotHandler", "DeviceManager", "BotLogger"]
